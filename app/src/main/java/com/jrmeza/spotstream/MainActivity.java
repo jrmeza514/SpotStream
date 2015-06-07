188bad899732bdb6e31cea981721c9632101d7e4
jrmeza <jrmeza514@gmail.com>
2015-06-07 15:06:04 -0700

    s

--- app/src/main/java/com/jrmeza/spotstream/MainActivity.java
+++ app/src/main/java/com/jrmeza/spotstream/MainActivity.java
@@ -132,7 +132,7 @@ public class MainActivity extends AppCompatActivity implements PlayerNotificatio
 
     @Override
     public void onLoggedOut() {
-        Toast.makeText(this, "Logged Out kk", Toast.LENGTH_LONG);
+        Toast.makeText(this, "Logged Out kk ", Toast.LENGTH_LONG);
     }
 
     @Override
