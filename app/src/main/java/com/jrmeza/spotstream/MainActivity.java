048157af27892e8804d0233e184bbea5c0a963f6
Juan Meza <jrmeza@Juans-MacBook-Pro.local>
2015-06-07 14:26:19 -0700

    Initial commit

---
+++ app/src/main/java/com/jrmeza/spotstream/MainActivity.java
@@ -0,0 +1,161 @@
+package com.jrmeza.spotstream;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.CompoundButton;
+import android.widget.Toast;
+import android.widget.ToggleButton;
+
+
+import com.spotify.sdk.android.authentication.AuthenticationClient;
+import com.spotify.sdk.android.authentication.AuthenticationRequest;
+import com.spotify.sdk.android.authentication.AuthenticationResponse;
+import com.spotify.sdk.android.player.Config;
+import com.spotify.sdk.android.player.ConnectionStateCallback;
+import com.spotify.sdk.android.player.Player;
+import com.spotify.sdk.android.player.PlayerNotificationCallback;
+import com.spotify.sdk.android.player.PlayerState;
+import com.spotify.sdk.android.player.PlayerStateCallback;
+import com.spotify.sdk.android.player.Spotify;
+
+public class MainActivity extends AppCompatActivity implements PlayerNotificationCallback, ConnectionStateCallback{
+    private static final  int LOGIN_REQUEST_CODE = 1337;
+    private static final String REDIRECT_URI = "jrmeza://callback";
+    private static final String CLIENT_ID = "356673a85fa140daadc9bef2efa38b5f";
+
+    private ToggleButton mToglgeButton;
+    private Player mPlayer;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        openLogin();
+        mToglgeButton = (ToggleButton) findViewById(R.id.tb1);
+        mToglgeButton.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if(isChecked){
+                    mPlayer.getPlayerState(new PlayerStateCallback() {
+                        @Override
+                        public void onPlayerState(PlayerState playerState) {
+                            //Toast.makeText(getApplicationContext(), playerState.trackUri, Toast.LENGTH_LONG).show();
+                            if (!playerState.trackUri.equals("") ){
+                                mPlayer.resume();
+                            }else{
+                                mPlayer.play("spotify:track:0zmfJIx6wH7zpFdIR8T8U9");
+                            }
+                        }
+                    });
+                }else{
+                    mPlayer.pause();
+                }
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+    protected void onActivityResult(int requestCode, int resultCode, Intent intent) {
+        super.onActivityResult(requestCode, resultCode, intent);
+
+        // Check if result comes from the correct activity
+        if (requestCode == LOGIN_REQUEST_CODE) {
+            AuthenticationResponse response = AuthenticationClient.getResponse(resultCode, intent);
+
+            if(response.getType() == AuthenticationResponse.Type.TOKEN) {
+
+                Config playerConfig = new Config(this, response.getAccessToken(), CLIENT_ID);
+
+                mPlayer = Spotify.getPlayer(playerConfig, this, new Player.InitializationObserver() {
+
+                    @Override
+                    public void onInitialized(Player player) {
+                        mPlayer.addConnectionStateCallback(MainActivity.this);
+
+                        mPlayer.addPlayerNotificationCallback(MainActivity.this);
+
+
+                    }
+
+                    @Override
+                    public void onError(Throwable throwable) {
+                        Log.e("MainActivity", "Could not initialize player: " + throwable.getMessage());
+                    }
+
+                });
+            }
+        }
+    }
+    private void openLogin(){
+        setContentView(R.layout.activity_main);
+
+        AuthenticationRequest.Builder builder = new AuthenticationRequest.Builder(CLIENT_ID, AuthenticationResponse.Type.TOKEN, REDIRECT_URI);
+
+        builder.setScopes(new String[]{"user-read-private", "streaming"});
+
+        AuthenticationRequest request = builder.build();
+
+        AuthenticationClient.openLoginActivity(this, LOGIN_REQUEST_CODE, request);
+    }
+
+    @Override
+    public void onLoggedIn() {
+        Toast.makeText(this, "Logged In", Toast.LENGTH_LONG);
+    }
+
+    @Override
+    public void onLoggedOut() {
+        Toast.makeText(this, "Logged Out", Toast.LENGTH_LONG);
+    }
+
+    @Override
+    public void onLoginFailed(Throwable throwable) {
+        Toast.makeText(this, "Failed To Log In!", Toast.LENGTH_LONG);
+    }
+
+    @Override
+    public void onTemporaryError() {
+
+    }
+
+    @Override
+    public void onConnectionMessage(String s) {
+
+    }
+
+    @Override
+    public void onPlaybackEvent(EventType eventType, PlayerState playerState) {
+
+    }
+
+    @Override
+    public void onPlaybackError(ErrorType errorType, String s) {
+        Toast.makeText(this, s, Toast.LENGTH_LONG);
+    }
+}
