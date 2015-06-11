ab98b6f0a84fc787974b5dfbc1f1ba768abe048f
jrmeza <jrmeza514@gmail.com>
2015-06-10 19:52:36 -0700

    Updated to stream the first result yielded by the text search.

--- app/src/main/java/com/jrmeza/spotstream/MainActivity.java
+++ app/src/main/java/com/jrmeza/spotstream/MainActivity.java
@@ -32,17 +32,13 @@ import com.spotify.sdk.android.player.PlayerNotificationCallback;
 import com.spotify.sdk.android.player.PlayerState;
 import com.spotify.sdk.android.player.PlayerStateCallback;
 import com.spotify.sdk.android.player.Spotify;
-
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.io.Reader;
-import java.io.UnsupportedEncodingException;
 import java.net.HttpURLConnection;
 import java.net.URL;
 
